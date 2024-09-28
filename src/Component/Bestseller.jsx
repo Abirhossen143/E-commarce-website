e91ad17993ada7cea31f7a05e17bdb69f9a9eacf
Abir Hossen <abirhossen1050@gmail.com>
2024-09-28 21:57:42 +0600

    latestcollection

--- src/Component/Bestseller.jsx
+++ src/Component/Bestseller.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Shopcontext } from "./../Context/Shopcontex";
 import Title from "./Title";
-import PRoductitem from "./PRoductitem";
+import Productitem from "./Productitem";
 
 const Bestseller = () => {
   const { products } = useContext(Shopcontext);
@@ -23,7 +23,7 @@ const Bestseller = () => {
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grids-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
         {bestSeller.map((item, index) => (
-          <PRoductitem
+          <Productitem
             key={index}
             id={item._id}
             name={item.name}
--- src/Component/LatestCollection.jsx
+++ src/Component/LatestCollection.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Shopcontext } from "./../Context/Shopcontex";
 import Title from "./Title";
-import PRoductitem from "./PRoductitem";
+import Productitem from "./Productitem";
 
 const LatestCollection = () => {
   const { products } = useContext(Shopcontext);
@@ -25,7 +25,7 @@ const LatestCollection = () => {
       {/* Rendering PRoduct */}
       <div className="grid grid-cols-2 gap-4 gap-y-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {latestProduct.map((item, index) => (
-          <PRoductitem
+          <Productitem
             key={index}
             id={item._id}
             image={item.image}
